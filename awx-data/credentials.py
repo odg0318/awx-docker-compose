2d41fa34d9b79ca79868a3c3f0fd6706cbc696ac
odg0318 <odg0318@gmail.com>
2019-10-07 17:11:58 +0900

    update

--- awx-data/credentials.py
+++ awx-data/credentials.py
@@ -14,7 +14,7 @@ BROKER_URL = 'amqp://{}:{}@{}:{}/{}'.format(
     "guest",
     "guest",
     "rabbitmq",
-    "11211",
+    "5672",
     "awx")
 
 CHANNEL_LAYERS = {
@@ -26,7 +26,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '{}:{}'.format("memcached", "5672")
+        'LOCATION': '{}:{}'.format("memcached", "11211")
     },
     'ephemeral': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
